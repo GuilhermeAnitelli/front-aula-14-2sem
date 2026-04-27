01af480443c20d058ab16b4daa1f6ef544b8752a
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-04-27 11:22:54 -0300

    chore : Criando estrutura das rotas e inicializando.

--- aula-11/src/main.tsx
+++ aula-11/src/main.tsx
@@ -1,9 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import Home from "./routes/Home/index.tsx";
+import Users from "./routes/Users/index.tsx";
+import Error from "./routes/Error/index.tsx";
+import { createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/users", element: <Users /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ aula-11/src/routes/Home/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+        <h1>Main</h1>
+    </main>
+  )
+}
